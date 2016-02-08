3e53bec2d3ee6d3513be4801d5a58c24e7c68849
Yuichi Hagio <yhagio87@gmail.com>
2016-02-07 21:39:03 -0500

    Complete interactivity

---
+++ README.md
@@ -0,0 +1,11 @@
+### Data Visualization with D3 - Bar Chart of GDP
+
+# [DEMO](http://yhagio.github.io/3-bar-chart)
+
+### Data Source
+- [U.S.A. - Gross Domestic Product ](https://research.stlouisfed.org/fred2/data/GDP.txt)
+- https://raw.githubusercontent.com/FreeCodeCamp/ProjectReferenceData/master/GDP-data.json
+
+### Resources
+ - [D3 API](https://github.com/mbostock/d3/wiki/API-Reference)
+ - [D3-Ch.10 - Interactivity eBook](http://chimera.labs.oreilly.com/books/1230000000345/ch10.html#_consideration_for_touch_devices)
--- main.js
+++ main.js
@@ -5,8 +5,12 @@ d3.json(data_url, function(error, json) {
     console.log('===Error===', error);
   }
 
+  // List of Month strings used to convert from number to text later
+  var monthNames = ["January", "February", "March", "April", "May", "June","July", "August", "September", "October", "November", "December"
+  ];
+
   var w = 1200;
-  var h = 500;
+  var h = 500 - 20;
 
   var minDate = new Date(json.data[0][0]);
   var maxDate = new Date(json.data[json.data.length - 1][0]);
@@ -18,6 +22,46 @@ d3.json(data_url, function(error, json) {
       width: w,
       height: h
     });
+
+// ===== Create X axis =====
+var xScale = d3.time.scale()
+  .domain([minDate, maxDate])
+  .range([0, 1000]);
+
+var xAxisCreate = d3.svg.axis()
+  .scale(xScale)
+  .orient('bottom')
+  .ticks(d3.time.years, 5);;
+
+var xAxis = svg.append('g')
+  .call(xAxisCreate)
+  .attr({
+    'class':'axis',
+    'transform': 'translate(50, ' + (h-20) + ')'
+  });
+
+// ===== Create Y axis =====
+var yScale = d3.scale.linear()
+  .domain([0, 20000])
+  .range([h, 0]);
+
+var yAxisCreate = d3.svg.axis()
+  .scale(yScale)
+  .orient('left');
+
+var yAxis = svg.append('g')
+  .call(yAxisCreate)
+  .attr({
+    'class': 'axis',
+    'transform': 'translate(50, -20)'
+  })
+  .append('text')
+    .attr('transform', 'rotate(-90)')
+    .attr('x', -30)
+    .attr('y', 5)
+    .attr('dy', '1em')
+    .style('text-anchor', 'end')
+    .text('Gross Domestic Product, USA');
 
   // ===== Create bar chart graph =====
   svg.selectAll('rect')
@@ -32,44 +76,34 @@ d3.json(data_url, function(error, json) {
          return h - ( (h * d[1]) / 20000 );
        },
        width: (w / json.data.length),
-       height: function(d) { return d[1]; },
+       height: function(d) {
+         return ( (h * d[1]) / 20000 );
+       },
        transform: 'translate(50, -20)',
        fill: '#3399CC'
+     })
+     // Creating tooltp on mouseover event
+     .on('mouseover', function(d) {
+       //Get this bar's x/y values, then augment for the tooltip
+      var xPosition = parseFloat(d3.select(this).attr("x"));
+      (xPosition > 900) ? xPosition = 900 : xPosition;
+      (xPosition < 50) ? xPosition = 50 : xPosition;
+      var yPosition = parseFloat(d3.select(this).attr("y")) / 2 + h / 2;
+
+      //Update the tooltip position and value
+      var thisDate = new Date(d[0]);
+      d3.select("#tooltip")
+        .style("left", xPosition + "px")
+        .style("top", yPosition + "px")
+        .select("#value")
+          .html('<strong>$'+d[1].toLocaleString()+' Billion</strong><p>'+thisDate.getFullYear()+' - '+monthNames[thisDate.getMonth()]+'</p>');
+
+      //Show the tooltip
+      d3.select("#tooltip").classed("hidden", false);
+     })
+     .on('mouseout', function(d) {
+       // Hide the tooltip on mouseout
+       d3.select('#tooltip').classed("hidden", true);
      });
-
-  // ===== Create X axis =====
-  var xScale = d3.time.scale()
-    .domain([minDate, maxDate])
-    .range([0, 1000]);
-
-  var xAxisCreate = d3.svg.axis()
-    .scale(xScale)
-    .orient('bottom')
-    .ticks(d3.time.years, 5);;
-
-  var xAxis = svg.append('g')
-    .call(xAxisCreate)
-    .attr({
-      'class':'axis',
-      'transform': 'translate(50, ' + (h-20) + ')'
-    });
-
-  // ===== Create Y axis =====
-  var yScale = d3.scale.linear()
-    .domain([0, 20000])
-    .range([h, 0]);
-
-  var yAxisCreate = d3.svg.axis()
-    .scale(yScale)
-    .orient('left');
-
-  var yAxis = svg.append('g')
-    .call(yAxisCreate)
-    .attr({
-      'class': 'axis',
-      'transform': 'translate(50, -20)'
-    });
-
-  // ===== Create tooltip =====
 
 });
